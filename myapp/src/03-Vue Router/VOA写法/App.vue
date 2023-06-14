b813f4ae206ba123ec5f6c22dfd9d3ae62ab5044
chenqiuli <sqiulia@163.com>
2023-06-14 17:51:10 +0800

    Vue Router VOA写法

---
+++ myapp/src/03-Vue Router/VOA写法/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <!-- 插槽-路由容器 -->
+    <router-view></router-view>
+    <TabBar />
+  </div>
+</template>
+
+<script>
+import TabBar from './components/Tabbar.vue';
+export default {
+  components: {
+    TabBar,
+  },
+};
+</script>
+
+<style>
+* {
+  padding: 0;
+  margin: 0;
+}
+body,
+html {
+  overflow-x: hidden;
+  overflow-y: auto;
+}
+li {
+  list-style: none;
+}
+a {
+  text-decoration: none;
+  color: unset !important;
+}
+</style>
